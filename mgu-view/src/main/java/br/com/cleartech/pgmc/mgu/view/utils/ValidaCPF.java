c98f2408ab8b4b89624277e3ed0407350c1943b2
rfernandes <rfernandes@cleartech.com.br>
2018-01-18 10:26:35 -0200

    ajustando validacao de CPf e tamanho do menu no layout

--- mgu-view/src/main/java/br/com/cleartech/pgmc/mgu/view/utils/ValidaCPF.java
+++ mgu-view/src/main/java/br/com/cleartech/pgmc/mgu/view/utils/ValidaCPF.java
@@ -5,7 +5,7 @@ import java.util.InputMismatchException;
 public class ValidaCPF {
 
 	public static boolean isCPF( String CPF ) {
-		CPF = CPF.replaceAll( ".", "" ).replaceAll( "-", "" );
+		CPF = CPF.replaceAll( "[^\\d]", "" );
 		// considera-se erro CPF's formados por uma sequencia de numeros iguais
 		if ( CPF.equals( "00000000000" ) || CPF.equals( "11111111111" ) || CPF.equals( "22222222222" ) || CPF.equals( "33333333333" ) || CPF.equals( "44444444444" ) || CPF.equals( "55555555555" ) || CPF.equals( "66666666666" ) || CPF.equals( "77777777777" ) || CPF.equals( "88888888888" ) || CPF.equals( "99999999999" ) || ( CPF.length() != 11 ) )
 			return false;
