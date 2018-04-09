1c9d435e9521e259d0d097efd2ab1cb5b508894d
rfernandes <rfernandes@cleartech.com.br>
2018-04-09 12:21:13 -0300

    restauranto alguns arquivos do .settings para permitir deploy no eclipse em todas as versoes

--- mgu-service/src/main/java/br/com/cleartech/pgmc/mgu/services/impl/GrupoPerfilServiceImpl.java
+++ mgu-service/src/main/java/br/com/cleartech/pgmc/mgu/services/impl/GrupoPerfilServiceImpl.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import br.com.cleartech.pgmc.mgu.entities.GrupoPerfil;
 import br.com.cleartech.pgmc.mgu.repositories.GrupoPerfilRepository;
 import br.com.cleartech.pgmc.mgu.services.GrupoPerfilService;
 
-@Component
+@Service
 public class GrupoPerfilServiceImpl implements GrupoPerfilService {
 
 	@Autowired
